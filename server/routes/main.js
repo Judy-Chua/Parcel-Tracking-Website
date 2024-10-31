eee4db67d8ace288b94d83b24b24b42d096c9a82
ItsMeeea <itsmeeeaemail@gmail.com>
2024-10-31 22:42:15 +0800

    branches, etc

--- server/routes/main.js
+++ server/routes/main.js
@@ -6,6 +6,10 @@ router.get('', async (req, res) =>{
     res.render('index', {title: "Home Page", css:"about_us"});
 })
 
+router.get('/branches', async (req, res) =>{
+    res.render('branches', {title: "Branches | ESMC", css: "branches_big"});
+})
+
 router.get('/contact_us', async (req, res) =>{
     res.render('contact_us', {title: "Contact Us | ESMC", css: "contact_us_big"});
 })
